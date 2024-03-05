1b6f8f3e996316cecc3011d74df2f9f6a98ccd5f
Daniel Meyer <dmeyer3583@conestogac.on.ca>
2024-03-05 11:46:12 -0500

    completed item logic

--- Midterm/Program.cs
+++ Midterm/Program.cs
@@ -9,7 +9,10 @@
     // Constructor
     public InventoryItem(string itemName, int itemId, double price, int quantityInStock)
     {
-        // TODO: Initialize the properties with the values passed to the constructor.
+        ItemName = itemName; 
+        ItemId = itemId;
+        Price = price;
+        QuantityInStock = quantityInStock;
     }
 
     // Methods
@@ -17,13 +20,13 @@
     // Update the price of the item
     public void UpdatePrice(double newPrice)
     {
-        // TODO: Update the item's price with the new price.
+        Price = newPrice;
     }
 
     // Restock the item
     public void RestockItem(int additionalQuantity)
     {
-        // TODO: Increase the item's stock quantity by the additional quantity.
+       QuantityInStock = QuantityInStock + additionalQuantity;
     }
 
     // Sell an item
@@ -31,18 +34,37 @@
     {
         // TODO: Decrease the item's stock quantity by the quantity sold.
         // Make sure the stock doesn't go negative.
+        if (IsInStock()) {
+            if (quantitySold <= QuantityInStock)
+            {
+                QuantityInStock = QuantityInStock - quantitySold;
+            }
+            else {
+                QuantityInStock = 0;
+            }
+        }
+
     }
 
     // Check if an item is in stock
     public bool IsInStock()
     {
-        // TODO: Return true if the item is in stock (quantity > 0), otherwise false.
+        if (QuantityInStock > 0)
+        {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+        
     }
 
     // Print item details
     public void PrintDetails()
     {
         // TODO: Print the details of the item (name, id, price, and stock quantity).
+        Console.WriteLine("Name:"+ItemName+"\nID:"+ItemId+"\nPrice:"+Price+"\nStock Quantity:"+QuantityInStock+"\n\n");
     }
 }
 class Program
@@ -53,13 +75,41 @@ class Program
         InventoryItem item1 = new InventoryItem("Laptop", 101, 1200.50, 10);
         InventoryItem item2 = new InventoryItem("Smartphone", 102, 800.30, 15);
 
+
         // TODO: Implement logic to interact with these objects.
         // Example tasks:
         // 1. Print details of all items.
         // 2. Sell some items and then print the updated details.
         // 3. Restock an item and print the updated details.
         // 4. Check if an item is in stock and print a message accordingly.
+        
+        
+        item1.PrintDetails();
+        item2.PrintDetails();
+        item1.SellItem(10);
+        item1.SellItem(10); 
+        item2.SellItem(10);
+        item1.PrintDetails();
+        item2.PrintDetails();
+        item2.RestockItem(5);
+        item2.PrintDetails();
 
-       
+
+        checkInStock(item1);
+        checkInStock(item2);
+
     }
+      static void checkInStock(InventoryItem item)
+      {
+
+        if (item.IsInStock())
+        {
+            Console.WriteLine("Item:" + item.ItemId + " " + item.ItemName + " Is in Stock");
+
+        }
+        else
+        {
+            Console.WriteLine("Item:" + item.ItemId + " " + item.ItemName + " Is not in Stock");
+        }
+      }
 }
